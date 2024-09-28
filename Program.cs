73295d88ddc78297b3e425336e1f1e56f94b6bf2
Erik Töger <erik.toger@gmail.com>
2024-09-28 21:07:38 +0200

    Add more things that fails on my other account

--- Program.cs
+++ Program.cs
@@ -1,13 +1,29 @@
-﻿using Pulumi;
-using Pulumi.AzureNative.Resources;
-using Pulumi.AzureNative.Storage;
-using Pulumi.AzureNative.Storage.Inputs;
-using System.Collections.Generic;
+﻿using Pulumi.AzureNative.Resources;
+using Pulumi.AzureAD;
 
 return await Pulumi.Deployment.RunAsync(() =>
 {
-    // Create an Azure Resource Group
-    var resourceGroup = new ResourceGroup("pulumi-test-RG");
+    var resourceGroup = new ResourceGroup("pulumi-test-RG", new ResourceGroupArgs
+    {
+        Location = "North Europe"
+    });
 
+    var config = new Pulumi.Config();
+    var myOwnId = config.Get("myOwnId") ?? "";
+    var applicationName = $"pulumi-test-application";
+    var application = new Application(applicationName, new ApplicationArgs
+    {
+        DisplayName = applicationName,
+        Owners = { myOwnId },
+    });
 
+    var federatednName = $"pulumi-test-federatedCredential";
+    var federatedCredential = new ApplicationFederatedIdentityCredential(federatednName, new ApplicationFederatedIdentityCredentialArgs
+    {
+        ApplicationId = application.Id,
+        Audiences = { "api://AzureADTokenExchange" },
+        DisplayName = federatednName,
+        Issuer = "https://token.actions.githubusercontent.com",
+        Subject = "repo:eriktoger/pulumi_test:environment:Production",
+    });
 });
